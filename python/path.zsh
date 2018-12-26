a182543d8b1657a45d338dd6b10215f6a570dcc2
Cluckerino <elitedragonslayer@gmail.com>
2018-12-25 22:08:49 -0800

    Added python env

---
+++ python/path.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+#
+# Setup python environment here
+#
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_SCRIPT=virtualenvwrapper.sh
+export WORKON_HOME=~/.pyenv
