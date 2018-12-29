7887eb5cb250389015e809b7296f5fc5cfb63f83
Cluckerino <elitedragonslayer@gmail.com>
2018-12-29 00:55:23 -0800

    More explicit path to virtualenvwrapper

--- python/path.zsh
+++ python/path.zsh
@@ -3,5 +3,5 @@
 # Setup python environment here
 #
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_SCRIPT=virtualenvwrapper.sh
+export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
 export WORKON_HOME=~/.pyenv
