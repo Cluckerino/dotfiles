8639ebb0c98e0b6da19e9c506a1b2b610358f79f
Cluckerino <elitedragonslayer@gmail.com>
2019-01-02 11:01:04 -0800

    Change dock behavior

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,6 +1,15 @@
+#
 # Sets reasonable macOS defaults.
-# 
-# Eventually, I'll care enough to use this.
+#
 
 # Disable press-and-hold for keys in favor of key repeat.
 #defaults write -g ApplePressAndHoldEnabled -bool false
+
+## Adjust dock size and behavior
+# Small icons
+defaults write com.apple.dock tilesize -float 40
+# Magnify to big icons
+defaults write com.apple.dock magnification -boolean YES
+defaults write com.apple.dock largesize -float 90
+
+killall Dock
