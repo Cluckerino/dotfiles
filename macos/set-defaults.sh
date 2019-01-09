a7a471250ce1a54b087f6568562cdbf8af0f138f
Cluckerino <elitedragonslayer@gmail.com>
2019-01-08 22:58:04 -0800

    Show hidden files in Finder

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -2,6 +2,9 @@
 # Sets reasonable macOS defaults.
 #
 
+# DAMMIT FINDER SHOW MY FILES
+defaults write com.apple.finder AppleShowAllFiles YES
+
 # Disable press-and-hold for keys in favor of key repeat.
 #defaults write -g ApplePressAndHoldEnabled -bool false
 
