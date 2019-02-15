ba47635c73737c061c762e82916a91456c417f2a
Cluckerino <elitedragonslayer@gmail.com>
2019-02-15 15:03:07 -0800

    Make sure iTerm2 profile folder exists

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -7,4 +7,5 @@ set -e
 cd "$(dirname "$0")/.."
 DOTFILES_ROOT=$(pwd -P)
 
+mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles/
 ln $DOTFILES_ROOT/iterm2/default_profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/linked_settings.json
