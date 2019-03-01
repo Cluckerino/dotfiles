ab9a78ae32f917c896560509afb5070166dc3f24
Cluckerino <elitedragonslayer@gmail.com>
2019-03-01 12:58:50 -0800

    I demand colors

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,3 +1,6 @@
+# Always colorful
+export TERM="xterm-256color"
+
 # Setup zsh syntax highlighting and auto suggestions
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
