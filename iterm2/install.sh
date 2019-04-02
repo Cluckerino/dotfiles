66f2dfb7c7ea912172dcb48793327fd748ad8088
Cluckerino <elitedragonslayer@gmail.com>
2019-04-02 07:10:33 -0700

    wow, make that symbolic please

--- iterm2/install.sh
+++ iterm2/install.sh
@@ -8,4 +8,4 @@ cd "$(dirname "$0")/.."
 DOTFILES_ROOT=$(pwd -P)
 
 mkdir -p ~/Library/Application\ Support/iTerm2/DynamicProfiles/
-ln $DOTFILES_ROOT/iterm2/default_profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/linked_settings.json
+ln -s $DOTFILES_ROOT/iterm2/default_profile.json ~/Library/Application\ Support/iTerm2/DynamicProfiles/linked_settings.json
