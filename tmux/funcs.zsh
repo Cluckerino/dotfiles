7615afcaa1c9a441575ac692bfeb508735749d05
Cluckerino <elitedragonslayer@gmail.com>
2019-05-03 22:04:47 -0700

    tmux funcs

---
+++ tmux/funcs.zsh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+#
+# Custom funcs
+#
+
+# Reload tmux.conf 
+alias tmuxconf='tmux source-file ~/.tmux.conf'
+
--- zsh/funcs.zsh
+++ zsh/funcs.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
+#
 # Custom funcs
+#
 
 # Make a directory then switch to it
 mkcd() { mkdir -p "$1" && cd "$1"; } 
