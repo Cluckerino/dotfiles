cb00a6ba54645b9ba598ba94c0fa24930c222385
Cluckerino <elitedragonslayer@gmail.com>
2020-08-25 17:54:37 -0700

    powerlevel10k

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,8 +7,8 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Install zsh addons as oh-my-zsh plugins
-# Install Powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# Install Powerlevel10k
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Install zsh-syntax-highlighting
